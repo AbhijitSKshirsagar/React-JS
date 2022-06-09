ed84f77b67d85f97473a97c544d591d8cc397ae2
AbhijitSKshirsagar <abhijitkshirsagar793@gmail.com>
2022-06-09 19:23:43 +0530

    added: ability to launch bridge labz site in a new tab on clicking bridgelabz logo

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './assets/logo.png';
 import './App.css';
 
 class App extends React.Component {
-
+  url = 'https://www.google.com'
   constructor(){
     super()
     this.state={
       title:'hello from bridgelabz'
     }
   }
+
+  onClick = ($event) => {
+    console.log("save button is click!", $event);
+    window.open(this.url, "blank");
+  }
   render(){
     return(
       <div>
         <h1>{this.state.title}</h1>
-        <img src={logo}
+        <img src={logo} onClick={this.onClick}
         alt="The BridgeLabz logo: a bridge to Employment through lab works"/>
       </div>
     )
